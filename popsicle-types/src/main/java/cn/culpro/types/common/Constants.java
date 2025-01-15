3ad115d70dd4031d13722630f7d601dbace81343
HogskinKitty <zymdev@126.com>
2025-01-15 18:21:05 +0800

    feat：增加常用工具类和通用类

--- popsicle-types/src/main/java/cn/culpro/types/common/Constants.java
+++ popsicle-types/src/main/java/cn/culpro/types/common/Constants.java
@@ -1,7 +1,30 @@
 package cn.culpro.types.common;
 
+/**
+ * 常量
+ *
+ * @author HogskinKitty
+ * @date 2025/01/15
+ */
 public class Constants {
     
+    /**
+     * 逗号
+     */
     public final static String SPLIT = ",";
     
+    /**
+     * 冒号
+     */
+    public final static String COLON = ":";
+    
+    /**
+     * 空格
+     */
+    public final static String SPACE = " ";
+    
+    /**
+     * 下划线
+     */
+    public final static String UNDERLINE = "_";
 }
